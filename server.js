dc6983b02ee797b57fdd18b52a96f190856d7fba
Samrawit Nebiyou <samrawit.merhatsidk@gmail.com>
2025-09-08 22:56:24 +0300

    Created Routes

--- server.js
+++ server.js
@@ -5,7 +5,16 @@ const app =express();
 //connect database
 connectDB();
 
-app.get('/',(req,res)=>res.send('API Running'))
+app.get('/',(req,res)=>res.send('API Running'));
+
+//Define Routes
+
+app.use('/api/users',require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts',require('./routes/api/posts'));
+
+
 
 const PORT=process.env.PORT || 5000;
 
