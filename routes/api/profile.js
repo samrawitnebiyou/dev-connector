e602a0d66839c58a8155e2a88f96a580234df95a
Samrawit Nebiyou <samrawit.merhatsidk@gmail.com>
2025-09-14 14:37:06 +0300

    Update profile.js

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -137,4 +137,26 @@ router.get('/user/:user_id', async (req, res) => {
   }
 });
 
+
+// @route    DELETE api/profile
+// @desc     Delete profile, user & post
+// @access   Private
+router.delete('/', auth,async (req, res) => {
+  try {
+    //@todo - remove users posts
+    //Remove profile
+    await Profile.findOneAndDelete({user:req.user.id});
+    //Remove user
+    await User.findOneAndDelete({_id:req.user.id});
+
+    res.json({msg:'User deleted'});
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+
+
+  }
+});
+
+
 module.exports = router;
